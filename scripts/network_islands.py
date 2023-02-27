8415c7c81bf6ff679eac83dc8a5276e56a8bfb77
Mark Morley <mmorley0395@gmail.com>
2023-02-27 17:46:10 -0500

    feat: create nodes from network for later use w isochrones

--- scripts/network_islands.py
+++ scripts/network_islands.py
@@ -20,9 +20,32 @@ generate_islands(
 stresslist = [2, 3, 4]
 for value in stresslist:
     db.execute(f"drop table if exists lts_stress_below_{value};")
-
+#
 gapslist = [1, 2, 3]
 for value in gapslist:
     db.execute(
-        f"create or replace view lts{value}gaps as select * from public.lts_full lf where lf.lts_score::int > {value}"
+            
+        f"""drop table if exists lts{value}gaps CASCADE;
+            create table lts{value}gaps as select * from public.lts_full lf where lf.lts_score::int > 2;
+            alter table lts{value}gaps add column source integer;
+            alter table lts{value}gaps add column target integer;
+            select pgr_createTopology('lts{value}gaps', 0.0005, 'geom', 'dvrpc_id');
+            create or replace view lts{value}nodes as 
+                select id, st_centroid(st_collect(pt)) as geom
+                from (
+                    (select source as id, st_startpoint(geom) as pt
+                    from lts{value}gaps
+                    ) 
+                union
+                (select target as id, st_endpoint(geom) as pt
+                from lts{value}gaps
+                ) 
+                ) as foo
+                group by id;
+            alter table lts{value}gaps add column length_m integer;
+            update lts{value}gaps set length_m = st_length(st_transform(geom,26918));
+            alter table lts{value}gaps add column traveltime_min double precision;
+            update lts{value}gaps set traveltime_min = length_m  / 16000.0 * 60; -- 16 kms per hr, about 10 mph. low range of beginner cyclist speeds
+
+            """
     )
