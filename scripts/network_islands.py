ff2c9a1ceed7d48a89ca7410abab54c37be0f689
Mark Morley <mmorley0395@gmail.com>
2023-03-13 11:06:23 -0400

    updated after reinstall of pgrouting, pointing to fdwgis now

--- scripts/network_islands.py
+++ scripts/network_islands.py
@@ -16,34 +16,29 @@ generate_islands(
     db, "lts_stress_below_4", "lts_3"
 )  # generates islands only of lts 4 segments
 
-# table cleanup, gets rid of interim tables
-stresslist = [2, 3, 4]
-for value in stresslist:
-    db.execute(f"drop table if exists lts_stress_below_{value};")
-#
 gapslist = [1, 2, 3]
 for value in gapslist:
     db.execute(
             
         f"""drop table if exists lts{value}gaps CASCADE;
-            create table lts{value}gaps as select * from public.lts_full lf where lf.lts_score::int > 2;
+            create table lts{value}gaps as select * from fdw_gis.lts_full lf where lf.lts_score::int > 2;
             alter table lts{value}gaps add column source integer;
             alter table lts{value}gaps add column target integer;
             select pgr_createTopology('lts{value}gaps', 0.0005, 'geom', 'dvrpc_id');
             create or replace view lts{value}nodes as 
                 select id, st_centroid(st_collect(pt)) as geom
                 from (
-                    (select source as id, st_startpoint(geom) as pt
+                    (select source as id, st_startpoint(shape) as pt
                     from lts{value}gaps
                     ) 
                 union
-                (select target as id, st_endpoint(geom) as pt
+                (select target as id, st_endpoint(shape) as pt
                 from lts{value}gaps
                 ) 
                 ) as foo
                 group by id;
             alter table lts{value}gaps add column length_m integer;
-            update lts{value}gaps set length_m = st_length(st_transform(geom,26918));
+            update lts{value}gaps set length_m = st_length(st_transform(shape,26918));
             alter table lts{value}gaps add column traveltime_min double precision;
             update lts{value}gaps set traveltime_min = length_m  / 16000.0 * 60; -- 16 kms per hr, about 10 mph. low range of beginner cyclist speeds
 
