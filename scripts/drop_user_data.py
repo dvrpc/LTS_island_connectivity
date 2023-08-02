323564fae22a7d5d977ff577c3a2ec85a3ad0343
Mark Morley <mmorley0395@gmail.com>
2023-08-02 17:43:41 -0400

    handy script during dev only while testing segments

---
+++ scripts/drop_user_data.py
@@ -0,0 +1,9 @@
+from pg_data_etl import Database
+
+# this file should not be used after production is launched! only useful while testing segments in early dev stages.
+db = Database.from_config("lts", "localhost")
+
+if __name__ == "__main__":
+    for value in ["sidewalk", "lts"]:
+        for table in ["blobs", "buffers", "islands", "segments", "isochrones"]:
+            db.execute(f"drop table if exists {value}.user_{table}")
