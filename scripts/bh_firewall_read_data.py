54f2971dbd83669279f55271fc98214eea3558b0
Mark Morley <mmorley0395@gmail.com>
2023-09-22 14:47:21 -0400

    schema creation working w sqlalchemy

--- scripts/bh_firewall_read_data.py
+++ scripts/bh_firewall_read_data.py
@@ -11,15 +11,27 @@ be used to populate your db with a copy of what is here.
 """
 
 import os
-from settings import URI, HOST, USER, PASSWORD, DB_NAME, GIS_HOST, GIS_USER, GIS_PASSWORD, GIS_DB_NAME, GIS_PORT
-from sqlalchemy import create_engine
+from settings import (
+    DATABASE_URL,
+    HOST,
+    USER,
+    PASSWORD,
+    DB_NAME,
+    GIS_HOST,
+    GIS_USER,
+    GIS_PASSWORD,
+    GIS_DB_NAME,
+    GIS_PORT,
+)
+from sqlalchemy import create_engine, text
 
-engine = create_engine(f'{URI}')
+engine = create_engine(f"{DATABASE_URL}")
+print(DATABASE_URL)
 
 
-def create_schemas(db):
-    engine.execute(
-        """
+def create_schemas(engine):
+    connection = engine.connect()
+    query = """
     CREATE extension if not exists postgis;
     CREATE extension if not exists pgrouting;
     DROP SCHEMA if exists summaries CASCADE;
@@ -30,7 +42,17 @@ def create_schemas(db):
     CREATE SCHEMA if not exists lts;
     DROP SCHEMA if exists connect_users cascade;
     CREATE SCHEMA if not exists connect_users;"""
-    )
+
+    transaction = connection.begin()
+
+    try:
+        connection.execute(text(query))
+        transaction.commit()
+    except:
+        transaction.rollback()
+        raise
+    finally:
+        connection.close()
 
 
 def import_data(
@@ -41,6 +63,7 @@ def import_data(
     imports data by creating copies of all tables.
     necessary to copy because of server location; no fdw.
     """
+    connection = engine.connect()
 
     print(f"initiating import of {full_layer_tablename}, please wait...")
 
@@ -48,23 +71,28 @@ def import_data(
         f"""ogr2ogr -lco GEOMETRY_NAME=geom -sql "{sql_query}" -explodecollections -f "PostgreSQL" -overwrite PG:"host={HOST} user={USER} dbname={DB_NAME} password={PASSWORD}" -t_srs "EPSG:26918" -f "PostgreSQL" PG:"host={GIS_HOST} port={GIS_PORT} dbname={GIS_DB_NAME} user={GIS_USER} password={GIS_PASSWORD}" -nln {full_layer_tablename}"""
     )
 
+    connection.close()
+
 
 def make_low_stress_lts(lts_level: int = 3):
     """Make a low stress network based on a certain threshold. Returns LTS network with
     all segments below specified lts_level (i.e. if write 'lts_level=3', it will create select LTS 1 and 2 as a new table.)
     """
+    connection = engine.connect()
     print(f"creating low stress bike network for lts_{lts_level}...")
-    engine.execute(
+    connection.execute(
         f"""
         drop table if exists lts.lts_stress_below_{lts_level};
         create table lts.lts_stress_below_{lts_level} as(
         select * from lts.lts_full where lts_score::int < {lts_level});
         """
     )
+    connection.close()
 
 
 def make_bike_ped_crash_view():
-    engine.execute(
+    connection = engine.connect()
+    connection.execute(
         """
         drop materialized view if exists bikepedcrashes;
         CREATE MATERIALIZED VIEW bikepedcrashes as 
@@ -76,10 +104,12 @@ def make_bike_ped_crash_view():
 
         """
     )
+    connection.close()
 
 
 def setup_user_table():
-    engine.execute(
+    connection = engine.connect()
+    connection.execute(
         """
         create table if not exists connect_users.users(
                     id SERIAL primary key,
@@ -89,63 +119,64 @@ def setup_user_table():
                 );    
         """
     )
+    connection.close()
 
 
 if __name__ == "__main__":
     create_schemas(engine)
-    import_data(
-        "select *, gid as dvrpc_id from transportation.lts_network",
-        "lts.lts_full",
-    )
-    import_data(
-        """select * from demographics.deccen_2020_block db
-            inner join demographics.census_blocks_2020 cb
-            on cb.geoid20 = db.geocode
-            where cb.dvrpc_reg = 'y'
-            """,
-        "censusblock2020_demographics",
-    )
-    import_data(
-        "select * from transportation.pedestriannetwork_lines where feat_type != 'UNMARKED'",
-        "sidewalk.ped_network",
-    )
-    import_data(
-        "select * from transportation.pedestriannetwork_gaps",
-        "sidewalk.ped_network_gaps",
-    )
-    import_data(
-        "select * from boundaries.municipalboundaries",
-        "municipalboundaries",
-    )
-    import_data(
-        "select * from planning.eta_essentialservicespts",
-        "essential_services",
-    )
-    import_data(
-        "select * from transportation.circuittrails",
-        "circuittrails",
-    )
-    import_data(
-        "select * from transportation.crash_newjersey",
-        "crash_newjersey",
-    )
-    import_data(
-        "select * from transportation.crash_nj_pedestrians",
-        "nj_ped_crash",
-    )
-    import_data(
-        "select * from transportation.passengerrailstations",
-        "passengerrailstations",
-    )
-    import_data(
-        "select * from planning.dvrpc_landuse_2015",
-        "landuse_2015",
-    )
-    import_data(
-        "select * from economy.nets_2015",
-        "nets_2015",
-    )
-    make_bike_ped_crash_view()
-    make_low_stress_lts(4)
-    make_low_stress_lts(3)
-    make_low_stress_lts(2)
+#     import_data(
+#         "select *, gid as dvrpc_id from transportation.lts_network",
+#         "lts.lts_full",
+#     )
+#     import_data(
+#         """select * from demographics.deccen_2020_block db
+#             inner join demographics.census_blocks_2020 cb
+#             on cb.geoid20 = db.geocode
+#             where cb.dvrpc_reg = 'y'
+#             """,
+#         "censusblock2020_demographics",
+#     )
+#     import_data(
+#         "select * from transportation.pedestriannetwork_lines where feat_type != 'UNMARKED'",
+#         "sidewalk.ped_network",
+#     )
+#     import_data(
+#         "select * from transportation.pedestriannetwork_gaps",
+#         "sidewalk.ped_network_gaps",
+#     )
+#     import_data(
+#         "select * from boundaries.municipalboundaries",
+#         "municipalboundaries",
+#     )
+#     import_data(
+#         "select * from planning.eta_essentialservicespts",
+#         "essential_services",
+#     )
+#     import_data(
+#         "select * from transportation.circuittrails",
+#         "circuittrails",
+#     )
+#     import_data(
+#         "select * from transportation.crash_newjersey",
+#         "crash_newjersey",
+#     )
+#     import_data(
+#         "select * from transportation.crash_nj_pedestrians",
+#         "nj_ped_crash",
+#     )
+#     import_data(
+#         "select * from transportation.passengerrailstations",
+#         "passengerrailstations",
+#     )
+#     import_data(
+#         "select * from planning.dvrpc_landuse_2015",
+#         "landuse_2015",
+#     )
+#     import_data(
+#         "select * from economy.nets_2015",
+#         "nets_2015",
+#     )
+#     make_bike_ped_crash_view()
+#     make_low_stress_lts(4)
+#     make_low_stress_lts(3)
+#     make_low_stress_lts(2)
