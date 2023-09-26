3631f6f4ec61a9b1b4fa98bb946c786c77f7f558
Mark Morley <mmorley0395@gmail.com>
2023-09-26 17:11:25 -0400

    add module reqs

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="lts-island-connectivity",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "geoalchemy2",
+        "geopandas",
+        "network-routing @ git+https://github.com/dvrpc/network-routing",
+        "pandana",
+        "pg-data-etl @ git+https://github.com/mmorley0395/pg-data-etl",
+        "pip-chill",
+        "psycopg2-binary",
+        "python-dotenv",
+        "tqdm",
+
+    ],
+)
